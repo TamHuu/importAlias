6b2a47ef963498f7162f2525f95b71c2126c57f7
HuuTam <christopherle0902@gmail.com>
2022-10-14 14:32:02 +0700

    importalias

---
+++ src/components/BasicButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default function BasicButton() {
+  return (
+    <div>
+      hahahahahahah
+      <button>Clicjk me</button>
+      <button>Clicjk me</button>
+      <button>Clicjk me</button>
+    </div>
+  );
+}
